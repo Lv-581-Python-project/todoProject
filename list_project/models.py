89d61f1481a4bfbf7ae6153df90b27d5cf26047a
OstapOmelchuk <ostap.omelchuk@gmail.com>
2021-03-10 16:31:04 +0200

    Change user package

--- list_project/models.py
+++ list_project/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-# from User.models import CustomUser
+# from user.models import User
 
 class List(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    # Change custom user on user table name
-    # user_id = models.ForeignKey(CustomUser)
+    # user_id = models.ForeignKey(User)
