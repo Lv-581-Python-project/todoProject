fd9ee0a389214a63c6e5b24a6053fa5bb5f72f93
SLDem <slavko.dem@gmail.com>
2021-03-10 15:25:14 +0200

    Added app to installed apps, added the urls and renamed List to ToDoList in the views

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.views.generic import DetailView, FormView, DeleteView, ListView
 from django.http import JsonResponse
 
 from .models import User
-from lists.models import List
+from lists.models import ToDoList
 
 
 class ProfileView(DetailView):
@@ -12,7 +12,7 @@ class ProfileView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.get_object()
-        lists = List.objects.filter(user_id=user.pk)
+        lists = ToDoList.objects.filter(user_id=user.pk)
         context['lists'] = lists
         context['user'] = user
         return context
