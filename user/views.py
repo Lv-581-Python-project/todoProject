55e51ea1e8351e82225f6254c1efde72d4bf38f8
SLDem <slavko.dem@gmail.com>
2021-03-10 21:25:17 +0200

    Changed ToDoList to List

--- user/views.py
+++ user/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import DetailView, FormView, DeleteView, ListView
+from django.views.generic import DetailView
 from django.http import JsonResponse
 
 from .models import User
-from lists.models import ToDoList
+from lists.models import List
 
 
 class ProfileView(DetailView):
@@ -12,7 +11,7 @@ class ProfileView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.get_object()
-        lists = ToDoList.objects.filter(user_id=user.pk)
+        lists = List.objects.filter(user_id=user.pk)
         context['lists'] = lists
         context['user'] = user
         return context
