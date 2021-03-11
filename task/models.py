f30ea572afd4b96783f033b9f7df330df5d544e0
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-03-11 14:53:51 +0200

    Fixed Task model, added first migration for Task model

--- task/models.py
+++ task/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-from django.conf import settings
 
 
-# Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=30,
-                             required=True)
+    title = models.CharField(max_length=30)
     description = models.TextField(max_length=256)
     is_completed = models.BooleanField(default=False)
-    deadline = models.DateField(required=True)
+    deadline = models.DateField()
 
     def __str__(self):
         return self.title
