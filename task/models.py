9df84243b46e16d47d9b74a536678853003e19cc
AndriiRomaniuk <suroviymarioboy@gmail.com>
2021-03-11 11:29:03 +0200

    Removed foreign keys from models

--- task/models.py
+++ task/models.py
@@ -1,30 +1,14 @@
 from django.db import models
 from django.conf import settings
-from list.models import ToDoList
+
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(
-        max_length=30,
-        required=True
-    )
-    description = models.TextField(
-        max_length=256
-    )
-    is_completed = models.BooleanField(
-        default=False
-    )
-    deadline = models.DateField(
-        required=True
-    )
-    user_id = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    list_id = models.ForeignKey(
-        ToDoList,
-        on_delete=models.CASCADE
-    )
+    title = models.CharField(max_length=30,
+                             required=True)
+    description = models.TextField(max_length=256)
+    is_completed = models.BooleanField(default=False)
+    deadline = models.DateField(required=True)
 
     def __str__(self):
         return self.title
