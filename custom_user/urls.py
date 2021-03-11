30c4e29ac14b11953e44901f44e9950913a62925
SLDem <slavko.dem@gmail.com>
2021-03-11 12:44:14 +0200

    Fixed for pylint

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:id>/', views.ProfileView, name='profile'),
+    path('profile/<int:user_id>/', views.ProfileView, name='profile'),
 ]
--- custom_user/views.py
+++ custom_user/views.py
@@ -1,14 +1,13 @@
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from .models import CustomUser
-from todolist.models import ToDoList
 
 
 class ProfileView(View):
 
-    def get(self, request, id=None):
-        user = CustomUser.find_by_id(id)
+    def get(self, request, user_id=None):
+        user = CustomUser.find_by_id(user_id)
         if user:
             return JsonResponse(user.to_dict())
-        return JsonResponse(status=400)
+        return HttpResponse(status=400)
