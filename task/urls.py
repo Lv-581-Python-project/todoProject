c7d98f857c03fc7b36abbadf456db0db5710ba9a
AndriiRomaniuk <suroviymarioboy@gmail.com>
2021-03-11 17:17:11 +0200

    minor edits

--- task/urls.py
+++ task/urls.py
@@ -4,6 +4,5 @@ from .views import TaskAPIView
 
 urlpatterns = [
     path('by_list/<int:list_id>', TaskAPIView.as_view()),
-    path('<int:task_id>', TaskAPIView.as_view()),
-    path('delete/<int:task_id>', TaskAPIView.as_view()),
+    path('', TaskAPIView.as_view()),
 ]
