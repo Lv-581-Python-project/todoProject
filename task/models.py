c38ab703152eda0cd3bdd6f546175a17ddda0790
AndriiRomaniuk <suroviymarioboy@gmail.com>
2021-03-12 11:16:44 +0200

    Migrations fix, updated task model

--- task/models.py
+++ task/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from custom_user.models import CustomUser
+from todolist.models import ToDoList
 
 
 class Task(models.Model):
@@ -6,6 +8,8 @@ class Task(models.Model):
     description = models.TextField(max_length=256)
     is_completed = models.BooleanField(default=False)
     deadline = models.DateField()
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    list_id = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
