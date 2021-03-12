ba7bc05ba95088ffaa0699387c8c4f107d754aa6
StepanTchynetskyi <tchynetsky@gmail.com>
2021-03-12 02:37:42 +0200

    change urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ToDoListView.as_view(), name='all-lists'),
     path('list/<int:todo_list_pk>/', views.ToDoListView.as_view(), name='one-list'),
-    path('<int:todo_list_pk>/delete/', views.ToDoListView.as_view(), name='delete-list'),
-    path('<int:todo_list_pk>/update/', views.ToDoListView.as_view(), name='update-list'),
-    path('create/', views.ToDoListView.as_view(), name='create-list')
+    path('delete-list/', views.ToDoListView.as_view(), name='delete-list'),
+    path('update-list/', views.ToDoListView.as_view(), name='update-list'),
+    path('create-list/', views.ToDoListView.as_view(), name='create-list')
 ]
