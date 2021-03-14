e81748fdeffb20c9547e6b0e3a868afd5fce331f
StepanTchynetskyi <tchynetsky@gmail.com>
2021-03-14 17:26:32 +0200

    Fix get method in ToDoListView

--- todolist/views.py
+++ todolist/views.py
@@ -19,7 +19,7 @@ class ToDoListView(View):
 
         todo_lists = ToDoList.get_all()
         todo_lists = json.dumps([todo_list.to_dict() for todo_list in todo_lists])
-        return JsonResponse(todo_lists, status=200)
+        return HttpResponse(todo_lists, status=200, content_type="application/json")
 
     def post(self, request):
         data = request.body
